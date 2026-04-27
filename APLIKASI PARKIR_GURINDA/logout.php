7a6dd4adbefaba56a80d29e74179e4017d46cb19
Gurindaaa <gurindafbri7@gmail.com>
2026-04-27 15:42:25 +0800

    Add files via upload

--- APLIKASI PARKIR_GURINDA/logout.php
+++ APLIKASI PARKIR_GURINDA/logout.php
@@ -1,1 +1,19 @@
-<p>helo</p>
+<?php
+session_start();
+include 'config/koneksi.php';
+include 'config/log.ph';
+
+// SIMPAN LOG SEBELUM SESSION DIHAPUS
+if(isset($_SESSION['id_user'])){
+    logAktivitas($conn, "Logout dari sistem");
+}
+/* Hapus semua session */
+session_unset();
+
+/* hancurkan session */
+session_destroy();
+
+/* Redirect ke login */
+header("Location: index.php");
+exit;
+?>
--- APLIKASI PARKIR_GURINDA/user/index.php
+++ APLIKASI PARKIR_GURINDA/user/index.php
@@ -1,1 +1,42 @@
-<p>halo</p>
+<?php
+include '../auth/cek_login.php';
+include '../auth/role.php';
+include '../config/koneksi.php';
+
+
+onlyAdmin();
+
+include '../template/header.php';
+include '../template/sidebar.php';
+include '../template/navbar.php';
+?>
+
+<div class="card p-3">
+     <div class="d-flex justify-content-between mb-3">
+           <h5>Data User</h5>
+           <a href="tambah.php" class="btn btn-primary"> + Tambah User</a>
+     </div>
+
+     <table class="table-hover">
+        <thead class="table-dark">
+            <tr>
+                <th>No</th>
+                <th>Nama</th>
+                <th>Username</th>
+                <th>Role</th>
+                <th>Status</th>
+                <th>Aksi</th>
+            </tr>
+        </thead>
+
+        <tbody>
+        <?php
+        $no=1;
+        $q = mysqli_query(&conn,"SELECT * FROM tb_user");
+
+        while($d = mysqli_fetch_assoc($q)){
+        ?>
+        
+        }
+        </tbody>
+     </table>
